af90446c8af55476f2980909daace8be0a7c90df
Maks <maxx4449@gmail.com>
2026-01-07 13:47:14 +0200

    Develop auth api feature

---
+++ authapi/src/test/java/com/maximys777/authapi/AuthapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maximys777.authapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ dataapi/src/main/java/com/maximys777/dataapi/service/TransformService.java
@@ -0,0 +1,26 @@
+package com.maximys777.dataapi.service;
+
+import com.maximys777.dataapi.exception.exceptions.IllegalArgumentException;
+import com.maximys777.dataapi.exception.exceptions.InvalidTokenException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransformService {
+
+    @Value("${app.internal-token}")
+    private String internalToken;
+
+    public String processText(String text, String tokenHeader) {
+
+        if (!internalToken.equals(tokenHeader)) {
+            throw new InvalidTokenException("Invalid token");
+        }
+
+        if (text == null) {
+            throw new IllegalArgumentException("Text cannot be null");
+        }
+
+        return new StringBuilder(text).reverse().toString();
+    }
+}
